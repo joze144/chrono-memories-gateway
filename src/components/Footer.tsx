ecbcae12f3a810aa0b557d741de13895b8705aad
fhf <710790342@qq.com>
2025-05-18 12:54:00 +0800

    update: update footer style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
 	return (
-		<footer className="px-4 border-t border-border/50 align-middle py-5 text-foreground/50">
+		<footer className="px-4 border-t border-border/50 align-middle py-5 text-foreground/50 text-center">
 			<p>© 2025 CryptoNote. All rights reserved.</p>
 			{/* <div className="container mx-auto">
 				<div className="mt-12 pt-8 text-sm text-foreground/50 flex flex-col md:flex-row justify-between items-center">
